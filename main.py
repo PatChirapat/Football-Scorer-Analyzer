df6a870d68787cdd4a764c88944e28270f5f1ab7
Chirapat <chirapat.ch@ku.th>
2024-05-02 21:38:47 +0700

    Running application file

---
+++ main.py
@@ -0,0 +1,6 @@
+from FSA_controller import FootballScorersAnalyzerCONTROLLER
+
+if __name__ == '__main__':
+    """display use interface"""
+    app = FootballScorersAnalyzerCONTROLLER()
+    app.run()
