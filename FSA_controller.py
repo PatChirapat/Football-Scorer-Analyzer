a20f479f6e936626a1c7963205cc67868577b523
Chirapat <chirapat.ch@ku.th>
2024-05-04 16:31:23 +0700

    Descriptive Statistics Featurre Completed

--- FSA_controller.py
+++ FSA_controller.py
@@ -1,10 +1,20 @@
 from FSA_model import FootballScorersAnalyzerMODEL
 from FSA_ui import FootballScorersAnalyzerUI
+import tkinter as tk
+from tkinter import ttk
+
 
 class FootballScorersAnalyzerCONTROLLER:
     def __init__(self):
         self.model = FootballScorersAnalyzerMODEL()
         self.view = FootballScorersAnalyzerUI(self)
+
+    def get_desciptive_statistics_keys(self):
+        descriptive_keys = self.model.describe_data()
+        return descriptive_keys
+
+    def get_descriptive_statistics_values(self, key):
+        return self.model.describe_data()[key]
 
     def run(self):
         self.view.run()
