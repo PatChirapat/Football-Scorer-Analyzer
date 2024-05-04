a9e88ed69eb216fae17fa45b37d233cb2036e997
Chirapat <chirapat.ch@ku.th>
2024-05-04 16:30:53 +0700

    Descriptive Statistics Feature Completed

--- FSA_model.py
+++ FSA_model.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
 class FootballScorersAnalyzerMODEL:
     def __init__(self):
-        pass
+        self.df = pd.read_csv("Football_Scoreres_Analyzer.csv")
+
+    def describe_data(self):
+        if "Year" in self.df.columns:
+            return self.df.drop(columns=["Year"]).select_dtypes(include=['int64', 'float64']).describe()
+        else:
+            return self.df.select_dtypes(include=['int64', 'float64']).describe()
+
